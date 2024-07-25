31ec6562fb19306a76efa8f80a42e66848fdd0ff
alaminthespecial <alaminthespecial@gmail.com>
2024-07-25 10:52:54 -0400

    update 4 errors commit

---
+++ app/components/LayoutProvider.js
@@ -0,0 +1,17 @@
+
+const LayoutProvider = ({ children }) => {
+    return (
+        <html lang="en">
+          <body >
+            
+    
+            {children}
+    
+    
+    
+          </body>
+        </html>
+      );
+}
+
+export default LayoutProvider
